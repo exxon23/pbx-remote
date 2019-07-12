b05ebfec2c034d13bd6ec9a6f4937a7978904e17
Sebastian Mach <sebastian.mach123@gmail.com>
2019-07-12 19:13:56 +0200

    Added connect status

--- src/containers/content.js
+++ src/containers/content.js
@@ -5,14 +5,22 @@ import { Context } from '../context/context'
 import './content.scss'
 
 const Content = () => {
-  const { activePage } = useContext(Context)
+  const { activePage, authorized } = useContext(Context)
   return (
-    <div className="pbx-remote__content-wrapper">
-      {activePage === 'home' && <Home />}
-      {activePage === 'phone' && <Dialpad />}
-      {activePage === 'inCall' && <Call />}
-      {activePage === 'ringing' && <Ringing />}
-    </div>
+    <>
+      <div className="pbx-remote__content-notification">
+        <span
+          className="pbx-remote__content-status"
+          style={{ backgroundColor: authorized ? '#AFBF27' : 'red' }}
+        />
+      </div>
+      <div className="pbx-remote__content-wrapper">
+        {activePage === 'home' && <Home />}
+        {activePage === 'phone' && <Dialpad />}
+        {activePage === 'inCall' && <Call />}
+        {activePage === 'ringing' && <Ringing />}
+      </div>
+    </>
   )
 }
 
